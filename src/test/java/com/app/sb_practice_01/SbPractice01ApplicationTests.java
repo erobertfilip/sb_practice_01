199b887af65be5b8d677a2f23750f50e50f9af5e
RobertFilip <robert.e.filip@gmail.com>
2023-10-28 18:26:27 +0300

    Applying SpringBoot Initializr template.

---
+++ src/test/java/com/app/sb_practice_01/SbPractice01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.sb_practice_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbPractice01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
